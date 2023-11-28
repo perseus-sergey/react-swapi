fefa94299f35b483063d22447c3813fbf79077c9
perseus <sergeygubriy@gmail.com>
2023-11-28 14:36:08 +0200

    fix: change path to home page

--- react-forms/src/main.tsx
+++ react-forms/src/main.tsx
@@ -5,7 +5,7 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import App from './App';
 import { ErrorRoutPage } from './components/ErrorFallback';
 import './index.css';
-import Home from './pages/home';
+import Home from './pages/Home';
 
 // export const store = setupStore();
 
